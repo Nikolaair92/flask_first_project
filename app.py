aa8b5b05e4e92095f97b2af0138a8e6a11d96206
Nikolaair92 <Karnauhov_07@mail.ru>
2021-01-28 11:47:20 +0300

    Modified app.py file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def index():
     return "Hello world"
 
@@ -12,6 +13,10 @@ def index():
 def about():
     return "About page"
 
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return "User page: " + name + " - " + str(id)
 
 if __name__ == "__main__":
     app.run(debug=True)
